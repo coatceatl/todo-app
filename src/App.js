48d161e0315aa5645903e8d21182f8573421f918
Oksana Tkachenko <oksana@crystax.net>
2018-03-24 12:29:42 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="todo-header">
+          <h1 className="todo-title">Todo App</h1>
+        </header>
+        <p className="App-intro">
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
