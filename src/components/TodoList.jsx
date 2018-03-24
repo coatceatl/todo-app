ba55c87b4b9e1881512c8a25979efc43c5442210
Oksana Tkachenko <oksana@crystax.net>
2018-03-24 15:02:20 +0300

    adds form for new task

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state={task: ''};
+
+    this.handleChange=this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    console.log('change');
+    this.setState({ task: e.target.value });
+  }
+
   render() {
+    const task = this.state.task;
     return (
       <div>
-        <p>TodoList</p>
+        <form>
+          <input
+            type='text'
+            value={task}
+            onChange={this.handleChange}
+          />
+          <button>New</button>
+        </form>
       </div>
     )
   }
